c3b6993995cf3fcdf67e5b4aece40b0fd25e4b08
David Terwilliger <terwilld@gmail.com>
2024-07-21 12:17:09 -0400

    render all products at /

--- routes/pages.js
+++ routes/pages.js
@@ -3,37 +3,55 @@ var router = express.Router();
 
 // Get Page model
 var Page = require('../models/page');
+var Product = require('../models/product');
+
+// Get Category model
+var Category = require('../models/category');
 
 /*
  * GET /
  */
 router.get('/', async function (req, res) {
-    currentPage = await Page.findOne({ slug: 'home' })
-    if (currentPage == null) {
-        console.log("no entry")
-        newPage = new Page(
-            {
-                slug: 'home',
-                title: 'Home',
-                content: 'Welcome to my page!'
-            }
-        )
 
-        await newPage.save()
-    }
-    Page.findOne({ slug: 'home' })
-        .then((page) => {
+    Product.find({})
+        .then((products) => {
+            res.render('all_products', {
+                title: 'All products',
+                products: products
+            });
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
-            //console.log(page)
-            //console.log(`Page Title: ${page.title}`)
-            //console.log(`Page content: ${page.content}`)
-            res.render('index', {
-                title: page.title,
-                content: page.content
-            })
-        }).catch((err) => {
-            console.log(err);
-        });
+
+
+    // currentPage = await Page.findOne({ slug: 'home' })
+    // if (currentPage == null) {
+    //     console.log("no entry")
+    //     newPage = new Page(
+    //         {
+    //             slug: 'home',
+    //             title: 'Home',
+    //             content: 'Welcome to my page!'
+    //         }
+    //     )
+
+    //     await newPage.save()
+    // }
+    // Page.findOne({ slug: 'home' })
+    //     .then((page) => {
+
+    //         //console.log(page)
+    //         //console.log(`Page Title: ${page.title}`)
+    //         //console.log(`Page content: ${page.content}`)
+    //         res.render('index', {
+    //             title: page.title,
+    //             content: page.content
+    //         })
+    //     }).catch((err) => {
+    //         console.log(err);
+    //     });
 
     // Page.findOne({slug: 'home'}, function (err, page) {
     //     if (err)
